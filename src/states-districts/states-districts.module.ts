be292fdaf46ddbde03021e54a68db40dbff42368
premnathkulal <premnathkulal1998@gmail.com>
2021-06-29 20:09:36 +0530

    sha256 token generating package installed (js-sha256)

--- src/states-districts/states-districts.module.ts
+++ src/states-districts/states-districts.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import { sha256 } from 'js-sha256'
 import { DistrictsSchema } from './schemas/districts.schema'
 import { StatesSchema } from './schemas/states.schema'
 import { StatesDistrictsController } from './states-districts.controller'
@@ -16,4 +17,9 @@ import { StatesDistrictsService } from './states-districts.service'
   controllers: [StatesDistrictsController],
   providers: [StatesDistrictsService, StatesDistrictsResolver],
 })
-export class StatesDistrictsModule {}
+export class StatesDistrictsModule {
+  constructor() {
+    const hashValue = sha256('992366')
+    console.log(hashValue)
+  }
+}
