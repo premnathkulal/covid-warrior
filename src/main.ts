bcc2fd8198442e8836a520e61222532cadde479a
premnathkulal <premnathkulal1998@gmail.com>
2021-07-08 19:42:53 +0530

    Setupfor deploying in Heroku

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   const config = new DocumentBuilder()
     .setTitle('Covid Warrior')
@@ -27,6 +28,6 @@ async function bootstrap() {
   writeFileSync('./swagger-spec.json', JSON.stringify(document))
   SwaggerModule.setup('api', app, document)
 
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
